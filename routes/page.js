3de34747de02fc441116aa712876510face71c3e
Trevor Behlman <tsbehlman@gmail.com>
2019-04-01 01:05:18 -0400

    Avoid enumerating all stories for each page

--- routes/page.js
+++ routes/page.js
@@ -1,3 +1,5 @@
+const { skip, take } = require( "rusty-iterator-utils" );
+
 const STORIES_PER_PAGE = 30;
 
 module.exports = ( async function() {
@@ -6,6 +8,6 @@ module.exports = ( async function() {
 	return function( pageIndex ) {
 		const end = stories.size - pageIndex * STORIES_PER_PAGE;
 		const start = end - STORIES_PER_PAGE;
-		return Array.from( stories.values() ).slice( start, end ).reverse();
+		return Array.from( take( skip( stories.values(), start ), end - start ) ).reverse();
 	}
 } )();
